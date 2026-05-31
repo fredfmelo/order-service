469492a2481c3939e9cd1ec53ffcf5345efd635d
Fred Melo <fredfmelo@gmail.com>
2026-05-31 18:12:09 -0300

    restoring OrderEventPublisher

---
+++ src/main/java/com/fredfmelo/orderservice/infrastructure/messaging/OrderEventPublisher.java
@@ -0,0 +1,54 @@
+package com.fredfmelo.orderservice.infrastructure.messaging;
+
+import java.util.Map;
+
+import org.springframework.stereotype.Component;
+
+import com.fredfmelo.eventdrivencore.exception.TechnicalException;
+import com.fredfmelo.eventdrivencore.outbox.publisher.OutboxEventPublisher;
+import com.fredfmelo.orderservice.config.ServiceConfig;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import software.amazon.awssdk.core.exception.SdkException;
+import software.amazon.awssdk.services.sns.SnsClient;
+import software.amazon.awssdk.services.sns.model.MessageAttributeValue;
+import software.amazon.awssdk.services.sns.model.PublishRequest;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class OrderEventPublisher implements OutboxEventPublisher{
+
+    private static final String EVENT_TYPE = "eventType";
+    private static final String STRING = "String";
+
+    private final SnsClient snsClient;
+    private final ServiceConfig config;
+
+    private Map<String, MessageAttributeValue> buildAttributes(String eventType) {
+        return Map.of(EVENT_TYPE,
+                MessageAttributeValue.builder()
+                        .dataType(STRING)
+                        .stringValue(eventType)
+                        .build());
+    }
+
+	@Override
+	public void publish(String payload, String eventType) {
+        try {
+            PublishRequest request = PublishRequest.builder()
+                    .topicArn(config.getSns().getOrderTopicArn())
+                    .message(payload)
+                    .messageAttributes(buildAttributes(eventType))
+                    .build();
+
+            snsClient.publish(request);
+
+            log.info("Published eventType={}", eventType);
+
+        } catch (SdkException ex) {
+            throw new TechnicalException("Error publishing SNS event", ex);
+        }
+	}
+}
